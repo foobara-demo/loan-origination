84700cdd2d75e1865e6a25009096b927624f7fb9
Miles Georgi <azimux@gmail.com>
2025-07-12 10:59:19 -0700

    Add more notes/experimentation to example scripts

--- bin/review_all_loan_files_two_agents.rb
+++ bin/review_all_loan_files_two_agents.rb
@@ -18,15 +18,21 @@ require_relative "../boot"
 
 module FoobaraDemo
   module LoanOrigination
+    class UnderwriterSummary < Foobara::Model
+      attributes do
+        loan_file_id :integer, :required
+        pay_stub_count :integer, :required
+        fico_scores [:integer, :integer, :integer], :required
+        credit_policy CreditPolicy, :required
+      end
+    end
+
     class ReviewLoanFile < Foobara::AgentBackedCommand
       description "Starts the LoanFile's review, then checks all requirements of its " \
                   "CreditPolicy and approves it if and only if all requirements are met. Otherwise denies it."
 
-      add_inputs do
-        loan_file LoanFile, :required
-      end
-
-      result LoanFile::UnderwriterDecision
+      inputs UnderwriterSummary
+      # result LoanFile::UnderwriterDecision
 
       depends_on StartUnderwriterReview,
                  FindCreditPolicy,
@@ -35,7 +41,8 @@ module FoobaraDemo
 
       verbose
       # works:
-      # llm_model  "claude-opus-4-20250514"
+      # llm_model "claude-opus-4-20250514"
+      # llm_model "claude-sonnet-4-20250514"
       agent_name "Inner"
       # llm_model "o1"
 
@@ -63,19 +70,20 @@ module FoobaraDemo
       end
 
       depends_on FindALoanFileThatNeedsReview,
-                 ReviewLoanFile
+                 ReviewLoanFile,
+                 FindLoanFile
       verbose
       agent_name "Outer"
 
       # works:
       # llm_model "claude-sonnet-4-20250514"
       # llm_model "claude-opus-4-20250514"
+      # llm_model "gpt-4o"
 
       # Does not work: can't construct the proper inputs to ReviewLoanFile for some reason
       # llm_model "deepseek-r1:70b"
 
       # llm_model "claude-3-7-sonnet-20250219"
-      # llm_model "gpt-4o"
       # llm_model "qwen3:8b"
       # llm_model "gpt-4o-mini"
 
@@ -83,6 +91,71 @@ module FoobaraDemo
       # llm_model "claude-3-sonnet-20240229"
       llm_model "gpt-4"
     end
+    # class ReviewLoanFile < Foobara::AgentBackedCommand
+    #   description "Starts the LoanFile's review, then checks all requirements of its " \
+    #               "CreditPolicy and approves it if and only if all requirements are met. Otherwise denies it."
+    #
+    #   add_inputs do
+    #     loan_file LoanFile, :required
+    #   end
+    #
+    #   result LoanFile::UnderwriterDecision
+    #
+    #   depends_on StartUnderwriterReview,
+    #              FindCreditPolicy,
+    #              DenyLoanFile,
+    #              ApproveLoanFile
+    #
+    #   verbose
+    #   # works:
+    #   # llm_model  "claude-opus-4-20250514"
+    #   agent_name "Inner"
+    #   # llm_model "o1"
+    #
+    #   # Does not seem to work... doesn't return proper json for denying a loan.
+    #   # llm_model "deepseek-r1:70b"
+    #   # Does not work: incorrectly denies Fumiko's loan file.
+    #   # llm_model "claude-sonnet-4-20250514"
+    #   # Incorrectly denies Fumiko
+    #   llm_model "claude-3-7-sonnet-20250219"
+    #   # Incorrectly denies Fumiko, incorrectly approves Basil
+    #   # llm_model "gpt-4o"
+    #
+    #   # llm_model "qwen3:8b"
+    #   # llm_model "gpt-4o-mini"
+    #
+    #   # pass_aggregates_to_llm
+    # end
+    #
+    # class ReviewAllLoanFiles < Foobara::AgentBackedCommand
+    #   description "Reviews all LoanFiles that need review."
+    #
+    #   result do
+    #     approved [LoanFile], :required
+    #     denied [LoanFile], :required
+    #   end
+    #
+    #   depends_on FindALoanFileThatNeedsReview,
+    #              ReviewLoanFile
+    #   verbose
+    #   agent_name "Outer"
+    #
+    #   # works:
+    #   # llm_model "claude-sonnet-4-20250514"
+    #   # llm_model "claude-opus-4-20250514"
+    #
+    #   # Does not work: can't construct the proper inputs to ReviewLoanFile for some reason
+    #   # llm_model "deepseek-r1:70b"
+    #
+    #   # llm_model "claude-3-7-sonnet-20250219"
+    #   # llm_model "gpt-4o"
+    #   # llm_model "qwen3:8b"
+    #   # llm_model "gpt-4o-mini"
+    #
+    #   # llm_model "o1"
+    #   # llm_model "claude-3-sonnet-20240229"
+    #   llm_model "gpt-4"
+    # end
   end
 end
 
