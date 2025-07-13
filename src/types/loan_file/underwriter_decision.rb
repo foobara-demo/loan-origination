590c28af3ef2ff97017ed2905da659978b80b429
Miles Georgi <azimux@gmail.com>
2025-07-13 15:22:28 -0700

    Add UnderwriterDecision#denied?

--- src/types/loan_file/underwriter_decision.rb
+++ src/types/loan_file/underwriter_decision.rb
@@ -1,7 +1,17 @@
 module FoobaraDemo
   module LoanOrigination
-    foobara_register_type(:decision, :string, one_of: [:approved, :denied])
-    foobara_register_type(:denied_reason, :string, one_of: [:insufficient_pay_stubs_provided, :low_credit_score])
+    module Decision
+      APPROVED = :approved
+      DENIED = :denied
+    end
+
+    module DeniedReason
+      INSUFFICIENT_PAY_STUBS_PROVIDED = :insufficient_pay_stubs_provided
+      LOW_CREDIT_SCORE = :low_credit_score
+    end
+
+    foobara_register_type(:decision, :symbol, one_of: Decision)
+    foobara_register_type(:denied_reason, :symbol, one_of: DeniedReason)
 
     class LoanFile < Foobara::Entity
       class UnderwriterDecision < Foobara::Model
@@ -9,6 +19,10 @@ module FoobaraDemo
           decision :decision, :required
           credit_score_used :integer, :required
           denied_reasons [:denied_reason], default: []
+        end
+
+        def denied?
+          decision == Decision::DENIED
         end
       end
     end
