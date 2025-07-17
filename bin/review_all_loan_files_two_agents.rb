3e20a0949c46131e7ca9ec67dcd4a81d6735ccb2
Miles Georgi <azimux@gmail.com>
2025-07-17 16:53:52 -0700

    Clean notes out of examples scripts

--- bin/review_all_loan_files_two_agents.rb
+++ bin/review_all_loan_files_two_agents.rb
@@ -40,24 +40,7 @@ module FoobaraDemo
                  ApproveLoanFile
 
       verbose
-      # works:
-      # llm_model "claude-opus-4-20250514"
-      # llm_model "claude-sonnet-4-20250514"
       agent_name "Inner"
-      # llm_model "o1"
-
-      # Does not seem to work... doesn't return proper json for denying a loan.
-      # llm_model "deepseek-r1:70b"
-      # Does not work: incorrectly denies Fumiko's loan file.
-      # llm_model "claude-sonnet-4-20250514"
-      # Incorrectly denies Fumiko
-      llm_model "claude-3-7-sonnet-20250219"
-      # Incorrectly denies Fumiko, incorrectly approves Basil
-      # llm_model "gpt-4o"
-
-      # llm_model "qwen3:8b"
-      # llm_model "gpt-4o-mini"
-
       # pass_aggregates_to_llm
     end
 
@@ -74,88 +57,8 @@ module FoobaraDemo
                  FindLoanFile
       verbose
       agent_name "Outer"
-
-      # works:
-      # llm_model "claude-sonnet-4-20250514"
-      # llm_model "claude-opus-4-20250514"
-      # llm_model "gpt-4o"
-
-      # Does not work: can't construct the proper inputs to ReviewLoanFile for some reason
-      # llm_model "deepseek-r1:70b"
-
-      # llm_model "claude-3-7-sonnet-20250219"
-      # llm_model "qwen3:8b"
-      # llm_model "gpt-4o-mini"
-
-      # llm_model "o1"
-      # llm_model "claude-3-sonnet-20240229"
       llm_model "gpt-4"
     end
-    # class ReviewLoanFile < Foobara::AgentBackedCommand
-    #   description "Starts the LoanFile's review, then checks all requirements of its " \
-    #               "CreditPolicy and approves it if and only if all requirements are met. Otherwise denies it."
-    #
-    #   add_inputs do
-    #     loan_file LoanFile, :required
-    #   end
-    #
-    #   result LoanFile::UnderwriterDecision
-    #
-    #   depends_on StartUnderwriterReview,
-    #              FindCreditPolicy,
-    #              DenyLoanFile,
-    #              ApproveLoanFile
-    #
-    #   verbose
-    #   # works:
-    #   # llm_model  "claude-opus-4-20250514"
-    #   agent_name "Inner"
-    #   # llm_model "o1"
-    #
-    #   # Does not seem to work... doesn't return proper json for denying a loan.
-    #   # llm_model "deepseek-r1:70b"
-    #   # Does not work: incorrectly denies Fumiko's loan file.
-    #   # llm_model "claude-sonnet-4-20250514"
-    #   # Incorrectly denies Fumiko
-    #   llm_model "claude-3-7-sonnet-20250219"
-    #   # Incorrectly denies Fumiko, incorrectly approves Basil
-    #   # llm_model "gpt-4o"
-    #
-    #   # llm_model "qwen3:8b"
-    #   # llm_model "gpt-4o-mini"
-    #
-    #   # pass_aggregates_to_llm
-    # end
-    #
-    # class ReviewAllLoanFiles < Foobara::AgentBackedCommand
-    #   description "Reviews all LoanFiles that need review."
-    #
-    #   result do
-    #     approved [LoanFile], :required
-    #     denied [LoanFile], :required
-    #   end
-    #
-    #   depends_on FindALoanFileThatNeedsReview,
-    #              ReviewLoanFile
-    #   verbose
-    #   agent_name "Outer"
-    #
-    #   # works:
-    #   # llm_model "claude-sonnet-4-20250514"
-    #   # llm_model "claude-opus-4-20250514"
-    #
-    #   # Does not work: can't construct the proper inputs to ReviewLoanFile for some reason
-    #   # llm_model "deepseek-r1:70b"
-    #
-    #   # llm_model "claude-3-7-sonnet-20250219"
-    #   # llm_model "gpt-4o"
-    #   # llm_model "qwen3:8b"
-    #   # llm_model "gpt-4o-mini"
-    #
-    #   # llm_model "o1"
-    #   # llm_model "claude-3-sonnet-20240229"
-    #   llm_model "gpt-4"
-    # end
   end
 end
 
